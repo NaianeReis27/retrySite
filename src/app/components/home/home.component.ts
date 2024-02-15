2b4a3dddaae79b5504cc73d0f2fec52eae0d970b
NaianeReis27 <email.annereis@gmail.com>
2024-02-15 17:10:14 -0300

    feat: add video

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    this.startCamera()
+  }
+  ratioFrame = 0.7
+  streams: any
+  isMobile() { return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent); };
 
+
+  handleStream(stream: any) { setTimeout(() => { let video: any = document.getElementById('video'); video.setAttribute('autoplay', ''); video.setAttribute('muted', ''); video.setAttribute('playsinline', ''); video.srcObject = stream; this.streams = stream.getVideoTracks(); }, 1000); };
+  startCamera() {
+    const navigatorControl: any = navigator; navigatorControl.getUserMedia = (navigatorControl as any).getUserMedia || (navigatorControl as any).webkitGetUserMedia || (navigatorControl as any).mozGetUserMedia || (navigatorControl as any).msGetUserMedia || (navigatorControl as any).mediaDevices.getUserMedia;
+    navigatorControl.mediaDevices.enumerateDevices().then((devices: any[]) => {
+      const videoDevices = devices.filter(device => device.kind === 'videoinput');
+      const constraints: any = { audio: false, video: { width: { exact: 640 }, height: { exact: 640 * this.ratioFrame } }, };
+      if (this.isMobile()) { constraints.video = { facingMode: 'enviroment', width: { exact: 640 * this.ratioFrame }, height: { exact: 640 }, }; }
+      for (const device of videoDevices) {
+        constraints.video.deviceId = device.deviceId; navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+          const track = stream.getVideoTracks()[0]; const capabilities: any = track.getCapabilities(); console.log(capabilities)
+          if (capabilities.focusMode && capabilities.facingMode && capabilities.focusMode.includes('continuous') && capabilities.facingMode.includes('enviroment')) { this.handleStream(stream); console.log("camera suporta focusMode === 'continuous"); return } else { track.stop(); }
+        })
+      };
+      delete constraints.video.deviceId;
+      navigator.mediaDevices.getUserMedia(constraints).then(stream => this.handleStream(stream)).catch(console.log)
+    })
+  }
 }
